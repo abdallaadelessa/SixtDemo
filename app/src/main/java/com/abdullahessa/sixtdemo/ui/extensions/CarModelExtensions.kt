2664d3def03e9317ca1834c05e6697765d8ae82a
abdullah.essa <abdullah.essa@check24.de>
2021-10-10 18:56:02 +0200

    Cleanup

--- app/src/main/java/com/abdullahessa/sixtdemo/ui/extensions/CarModelExtensions.kt
+++ app/src/main/java/com/abdullahessa/sixtdemo/ui/extensions/CarModelExtensions.kt
@@ -1,8 +1,6 @@
 package com.abdullahessa.sixtdemo.ui.extensions
 
-import androidx.annotation.StringRes
 import androidx.compose.ui.graphics.Color
-import com.abdullahessa.sixtdemo.R
 import com.abdullahessa.sixtdemo.domain.cars.model.CarModel.CarInfo
 import com.abdullahessa.sixtdemo.domain.cars.model.CarModel.FuelInfo
 
@@ -16,15 +14,6 @@ fun CarInfo.Cleanliness?.toColor(): Color =
         CarInfo.Cleanliness.VERY_CLEAN -> Color(0xFF388e3c)
         null -> Color.Red
     }
-
-@StringRes
-fun CarInfo.Cleanliness?.toStringResId(): Int =
-    when (this) {
-        CarInfo.Cleanliness.CLEAN -> R.string.clean
-        CarInfo.Cleanliness.VERY_CLEAN -> R.string.very_clean
-        else -> R.string.regular
-    }
-
 
 fun FuelInfo?.levelColor(): Color {
     val level = this?.fuelLevel ?: return Color.Red
